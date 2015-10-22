55eb7db6a5ac9d02dab402c61ebff9260537d2d6
unknown <Ravi Vyas>
2015-10-22 16:01:03 -0400

    added index.html

--- public/angular/app.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module("HelloWorldApp", []);
-
-app.controller("HelloWorldController", HelloWorldController);
-
-function HelloWorldController($scope)
-{
-    $scope.hello = "Hello World from AngularJS";
-}
