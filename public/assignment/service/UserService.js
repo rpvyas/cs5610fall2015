eb8edbb89efc2b3bc7f9c150e640b4df54e96551
unknown <Ravi Vyas>
2015-10-25 12:06:58 -0400

    modified user service

--- public/assignment/service/UserService.js
+++ public/assignment/service/UserService.js
@@ -1,3 +1,50 @@
-/**
- * Created by Ravi on 10/22/2015.
- */
+(function(){
+    angular
+        .module("FormBuilderApp")
+        .factory("UserService", UserService);
+
+    function UserService() {
+        var users = [
+
+        ];
+
+
+        //
+        var service = {
+            getAllCourses: getAllCourses
+        };
+        return service;
+
+        function createUser(user,callback)
+        {
+            users.push(user)
+                .success(callback);
+        }
+
+        function deleteUserById(id,callback)
+        {
+            var user = users.findOne({id:id});
+            var index = users.indexOf(user);
+            users.splice(index, 1)
+                .success(callback);
+        }
+
+        function updateUser(id,user,callback)
+        {
+
+            
+        }
+
+        function findUserByUsernameAndPassword(username,password,callback)
+        {
+            users.findOne({username:username, password:password})
+            .success(callback);
+
+        }
+
+        function findAllCourses(callback) {
+            return courses
+                .success(callback);
+        }
+    }
+})();
