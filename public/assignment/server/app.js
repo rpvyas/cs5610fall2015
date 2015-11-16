97e3735de5e3fca5fac89c7a7b9f328ce514b3dd
unknown <Ravi Vyas>
2015-11-16 16:37:12 -0500

    user service added

--- public/assignment/server/app.js
+++ public/assignment/server/app.js
@@ -3,7 +3,7 @@
 module.exports = function(app, mongoose, db)
 {
     var userModel = require("./models/user.model.js")(app, mongoose, db);
-    require("./services/user.service.server.js")(app, userModel, db);
+    require("./services/user.service.js")(app, userModel, db);
 
     var formModel = require("./models/form.model.js")(app, mongoose, db);
     require("./services/form.service.server.js")(app, formModel, db);
--- public/assignment/server/services/user.service.js
+++ public/assignment/server/services/user.service.js
@@ -1,5 +1,21 @@
 "use strict"
 
+//POST /api/assignment/user
+//creates a new user embedded in the body of the request, and responds with an array of all users
+//GET /api/assignment/user
+//responds with an array of all users
+//GET /api/assignment/user/:id
+//responds with a single user whose id property is equal to the id path parameter
+//GET /api/assignment/user?username=username
+//    responds with a single user whose username property is equal to the username path parameter
+//GET /api/assignment/user?username=alice&password=wonderland
+//    responds with a single user whose username property is equal to the username path parameter and its password is equal to the password path parameter
+//PUT /api/assignment/user/:id
+//updates an existing user whose id property is equal to the id path parameter. The new properties are set to the values in the user object embedded in the HTTP request. Responds with an array of all users
+//DELETE /api/assignment/user/:id
+
+
+
 module.exports = function(app,model)
 {
     app.post("/api/assignment/user", function(req,res)
@@ -14,6 +30,7 @@ module.exports = function(app,model)
 
         if(typeof username === 'undefined' && typeof password === 'undefined')
         {
+            //returning array of all users
             res.json(model.FindAll());
         }
         else if(username != null && password != null)
